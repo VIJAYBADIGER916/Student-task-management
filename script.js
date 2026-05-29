ba6f9b2873ee509bb2a051afc53e5cace7925548
Vijay Badiger <badigervijay766@gmail.com>
2026-05-29 12:50:32 +0530

    Initiliase task manager project

---
+++ script.js
@@ -0,0 +1,27 @@
+function addTask() {
+
+    const taskInput = document.getElementById("taskInput");
+    const taskText = taskInput.value;
+
+    if (taskText === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+        ${taskText}
+        <button class="delete-btn" onclick="deleteTask(this)">
+            Delete
+        </button>
+    `;
+
+    document.getElementById("taskList").appendChild(li);
+
+    taskInput.value = "";
+}
+
+function deleteTask(button) {
+    button.parentElement.remove();
+}
