58fa03f463b0d47350e9fd3b81edace0f63b5ffd
Vijay Badiger <badigervijay766@gmail.com>
2026-05-29 13:18:54 +0530

    Add localstorage functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", loadTasks);
+
 function addTask() {
 
     const taskInput = document.getElementById("taskInput");
@@ -8,20 +10,67 @@ function addTask() {
         return;
     }
 
+    createTaskElement(taskText);
+
+    saveTask(taskText);
+
+    taskInput.value = "";
+}
+
+function createTaskElement(taskText) {
+
     const li = document.createElement("li");
 
     li.innerHTML = `
-        ${taskText}
+        <span onclick="completeTask(this)">
+            ${taskText}
+        </span>
+
         <button class="delete-btn" onclick="deleteTask(this)">
             Delete
         </button>
     `;
 
     document.getElementById("taskList").appendChild(li);
-
-    taskInput.value = "";
 }
 
 function deleteTask(button) {
-    button.parentElement.remove();
+
+    const li = button.parentElement;
+    const taskText = li.querySelector("span").innerText;
+
+    removeTaskFromStorage(taskText);
+
+    li.remove();
+}
+
+function completeTask(task) {
+    task.classList.toggle("completed");
+}
+
+function saveTask(task) {
+
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    tasks.push(task);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasks() {
+
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    tasks.forEach(task => {
+        createTaskElement(task);
+    });
+}
+
+function removeTaskFromStorage(taskText) {
+
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    tasks = tasks.filter(task => task !== taskText);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
